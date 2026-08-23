a0d427fc8b18f33341c4f15c93f112884a960846
nahsir12390 <nahsir12390@users.noreply.github.com>
2026-08-23 18:46:33 +0100

    Seed database on container startup

--- docker-start.sh
+++ docker-start.sh
@@ -49,5 +49,6 @@ php artisan storage:link || true
 php artisan config:cache
 php artisan route:cache
 php artisan migrate --force
+php artisan db:seed --force --no-interaction
 
 exec php artisan serve --host 0.0.0.0 --port "${PORT:-8000}"
